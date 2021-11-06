79ef96a08d5b849894fc16bd6ad033b0fa900bff
lana99 <lana.abno20@gmail.com>
2021-11-06 15:01:55 +0100

    Added image, generater button and socialmedia button

--- meme-machine/src/App.js
+++ meme-machine/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+//import Button from './components/Button'
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  TwitterShareButton,
+  TwitterIcon,
+} from "react-share";
+
+
+export default class App extends Component {
+  render() {
+    const  shareUrl = 'https://www.youtube.com/watch?v=9WzIACv_mxs';
+    return (
+      <div style={{
+         background: '#0000',
+        height: "100vh",
+         width: "100%",
+        }}
+      >
+        <h3>Share your meme to Twitter! Created during Hackathon hold by Capgemini! KTH could Never </h3>
+        <img src="/images/heckling.jpg" alt=""/>
+        <TwitterShareButton url={shareUrl} >
+           <TwitterIcon size={40}/>
+        </TwitterShareButton>
+     </div>
+
+    );
+
+  }
 }
 
-export default App;
+
